d1c259c949c8a1b7c7e32d8572206ad73df25252
Nicola TEB <nicosia.k@interia.pl>
2025-02-04 09:42:07 +0100

    osoba

---
+++ ProgramowanieObiektowe/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektowe
+{
+    public class Osoba
+    {
+        protected string Imie { get; set; }
+        protected string Nazwisko { get; set; }
+        protected string DataUrodzenia { get; set; }
+
+        public Osoba(string imie, string nazwisko, string dataUrodzenia)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            DataUrodzenia = dataUrodzenia;
+        }
+
+        public void WypiszInfo()
+        {
+
+        }
+    }
+}
